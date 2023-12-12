9dc9df916ef93529a30974e5fd6fe6adbb414c76
Christopher Swenson <swenson@swenson.io>
2023-12-12 00:29:41 -0800

    Day 12

--- src/day12.rs
+++ src/day12.rs
@@ -1,6 +1,114 @@
-use std::ops::Index;
+use memoize::memoize;
 
 pub fn day12() {
+    let _inp = "???.### 1,1,3
+.??..??...?##. 1,1,3
+?#?#?#?#?#?#?#? 1,3,1,6
+????.#...#... 4,1,1
+????.######..#####. 1,6,5
+?###???????? 3,2,1";
+    let inp = std::fs::read_to_string("day12.input.txt").unwrap();
+    let inp = inp.trim();
+    let inp = inp.replace('.', "x");
+
+    let mut sum = 0;
+    for line in inp.lines() {
+        let line = line.trim_matches('x');
+        let line = line.replace("xx", "x");
+        sum += num_ways_start(&line);
+    }
+
+    println!("sum = {}", sum);
+
+    day12_2();
+}
+
+fn num_ways_start(line: &str) -> u64 {
+    let v: Vec<_> = line.split(' ').collect();
+    let pattern = v[0];
+    let nums: Vec<usize> = v[1].split(',').map(|x| x.parse().unwrap()).collect();
+
+    num_ways_pieces(pattern.to_string(), nums)
+}
+
+#[memoize]
+fn num_ways_pieces(pattern: String, nums: Vec<usize>) -> u64 {
+    let pattern = pattern.as_str();
+    // can we pull off a piece at the front?
+    if let Some(stripped) = pattern.strip_prefix('x') {
+        return num_ways_pieces(stripped.to_string(), nums);
+    }
+    if nums.is_empty() {
+        if pattern.contains('#') {
+            return 0;
+        }
+        return 1;
+    }
+
+    if pattern.starts_with('#') {
+        let mut x = 1;
+        while x < pattern.len() && pattern.as_bytes()[x] == u8::try_from('#').unwrap() {
+            x += 1;
+        }
+        // too many #
+        if x > nums[0] {
+            return 0;
+        }
+    }
+
+    let total: usize = nums.iter().copied().sum();
+    let mut left = 0;
+    for x in pattern.chars() {
+        if x == '?' || x == '#' {
+            left += 1;
+        }
+    }
+    // not enough choices left
+    if left == total {
+        let p2 = pattern.replace('?', "#");
+        if matches(&p2, &nums) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+    if left < total {
+        return 0;
+    }
+    if let Some(x) = pattern.find('x') {
+        if !&pattern[0..x].contains('?') {
+            if nums[0] == x {
+                return num_ways_pieces((pattern[x + 1..]).to_string(), nums[1..].to_vec());
+            } else {
+                return 0;
+            }
+        }
+    }
+    if matches(pattern, &nums) {
+        return 1;
+    }
+    if !pattern.contains('?') {
+        return 0;
+    }
+    let b = pattern.replacen('?', "#", 1);
+    let y = num_ways_pieces(b, nums.clone());
+    let a = pattern.replacen('?', "x", 1);
+    let x = num_ways_pieces(a, nums);
+    x + y
+}
+
+fn matches(pattern: &str, nums: &[usize]) -> bool {
+    if pattern.contains('?') {
+        return false;
+    }
+    nums == pattern
+        .split('x')
+        .map(|x| x.len())
+        .filter(|&x| x != 0)
+        .collect::<Vec<_>>()
+}
+
+pub fn day12_2() {
     let _inp = "???.### 1,1,3
 .??..??...?##. 1,1,3
 ?#?#?#?#?#?#?#? 1,3,1,6
@@ -12,91 +120,8 @@ pub fn day12() {
 
     let mut sum = 0;
     for line in inp.lines() {
-        let line = line.trim_matches('.');
-        let line = line.replace("..", ".");
-        sum += num_ways_start(&line);
-    }
-
-    println!("sum = {}", sum);
-
-    day12_2();
-}
-
-fn num_ways_start(line: &str) -> u64 {
-    let v: Vec<_> = line.split(' ').collect();
-    let mut pattern: Vec<char> = v[0].chars().collect();
-    let nums: Vec<usize> = v[1].split(',').map(|x| x.parse().unwrap()).collect();
-
-    num_ways(&mut pattern, &nums)
-}
-
-fn num_ways(pattern: &mut Vec<char>, nums: &[usize]) -> u64 {
-    // let p: String = pattern.iter().collect();
-    // println!("checking {}", p);
-    if matches(pattern, nums) {
-        // println!("  ok => 1");
-        return 1;
-    }
-    if !pattern.contains(&'?') {
-        // println!("  done");
-        return 0;
-    }
-    if !possible(pattern, nums) {
-        // println!("  impossible");
-        return 0;
-    }
-    let i = pattern.iter().enumerate().find(|&(_,&c)| c == '?').unwrap().0;
-    pattern[i] = '#';
-    let x = num_ways(pattern, nums);
-    pattern[i] = '.';
-    let y = num_ways(pattern, nums);
-    pattern[i] = '?';
-    x + y
-}
-
-// fn pieces(x: &str) -> Vec<&str> {
-//     x.split('.').iter().filter(|x| x.len() > 0).collect()
-// }
-
-fn possible(pattern: &Vec<char>, nums: &[usize]) -> bool {
-    let p: String = pattern.iter().collect();
-    let x: String = p.replace('?', ".");
-    let y: Vec<_> = x.split('.').filter(|&x| x.len() > 0 && !x.contains('?')).map(|x| x.len()).collect();
-    let first_q = p.find("?").unwrap_or(0);
-    let first_h = p.find("#").unwrap_or(0);
-    let z = *y.first().unwrap_or(&0);
-    if first_q > first_h && z > nums[0] {
-        //println!("  because first: {:?} vs {:?}", y, nums);
-        false
-    } else if y.iter().max().unwrap_or(&0) > nums.iter().max().unwrap() {
-        //println!("  because max");
-        false
-    } else {
-        true
-    }
-}
-
-fn matches(pattern: &Vec<char>, nums: &[usize]) -> bool {
-    if pattern.contains(&'?') {
-        return false;
-    }
-    nums == pattern.split(|&x| x == '.').map(|x| x.len()).filter(|&x| x != 0).collect::<Vec<_>>()
-}
-
-pub fn day12_2() {
-    let inp = "???.### 1,1,3
-.??..??...?##. 1,1,3";
-// ?#?#?#?#?#?#?#? 1,3,1,6
-// ????.#...#... 4,1,1
-// ????.######..#####. 1,6,5
-// ?###???????? 3,2,1";
-    let _inp = std::fs::read_to_string("day12.input.txt").unwrap();
-    let inp = inp.trim();
-
-    let mut sum = 0;
-    for line in inp.lines() {
-        sum += num_ways_start_unfold(&line);
-        println!("temp sum = {}", sum);
+        let n = num_ways_start_unfold(line);
+        sum += n;
     }
 
     println!("sum = {}", sum);
@@ -104,19 +129,9 @@ pub fn day12_2() {
 
 fn num_ways_start_unfold(line: &str) -> u64 {
     let v: Vec<_> = line.split(' ').collect();
-    let pattern = v[0];
-    let mut p2 = vec![];
-    for i in 0..5 {
-        for c in pattern.chars() {
-            p2.push(c);
-        }
-        if i != 4 {
-            p2.push('?');
-        }
-    }
-    let mut pattern = p2;
-    //let mut pattern = pattern.repeat(5).trim_matches('.').chars().collect();
+    let pattern = v[0].to_string() + "?" + v[0] + "?" + v[0] + "?" + v[0] + "?" + v[0];
+    let pattern = pattern.replace('.', "x");
     let nums: Vec<usize> = v[1].split(',').map(|x| x.parse().unwrap()).collect();
     let nums = nums.repeat(5);
-    num_ways(&mut pattern, &nums)
+    num_ways_pieces(pattern, nums.to_vec())
 }
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ extern crate core;
 mod day1;
 mod day10;
 mod day11;
+mod day12;
 mod day2;
 mod day3;
 mod day4;
@@ -11,7 +12,6 @@ mod day6;
 mod day7;
 mod day8;
 mod day9;
-mod day12;
 
 use clap::{Parser, Subcommand};
 use std::fmt;
