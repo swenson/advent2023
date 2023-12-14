40010eb8516f7f58e0ee4feda15b49a80f2a7b8b
Christopher Swenson <swenson@swenson.io>
2023-12-13 21:41:09 -0800

    Day 14 part 2

--- src/day14.rs
+++ src/day14.rs
@@ -1,3 +1,5 @@
+use memoize::memoize;
+
 pub fn day14() {
     let _inp = "O....#....
 O.OO#....#
@@ -12,7 +14,10 @@ O.#..O.#.#
     let inp = std::fs::read_to_string("day14.input.txt").unwrap();
     let inp = inp.trim();
 
-    let lines: Vec<_> = inp.lines().map(|x| x.chars().collect::<Vec<char>>()).collect();
+    let lines: Vec<_> = inp
+        .lines()
+        .map(|x| x.chars().collect::<Vec<char>>())
+        .collect();
 
     let lines = tilt_north(lines);
     let score = weight(lines);
@@ -43,13 +48,11 @@ fn tilt_north(v: Vec<Vec<char>>) -> Vec<Vec<char>> {
 
 fn tilt_north_once(v: Vec<Vec<char>>) -> Vec<Vec<char>> {
     let mut w = v.clone();
-    for i in 0..w.len()-1 {
+    for i in 0..w.len() - 1 {
         for j in 0..w[i].len() {
-            if w[i][j] == '.' {
-                if w[i + 1][j] == 'O' {
-                    w[i + 1][j] = '.';
-                    w[i][j] = 'O';
-                }
+            if w[i][j] == '.' && w[i + 1][j] == 'O' {
+                w[i + 1][j] = '.';
+                w[i][j] = 'O';
             }
         }
     }
@@ -57,8 +60,111 @@ fn tilt_north_once(v: Vec<Vec<char>>) -> Vec<Vec<char>> {
 }
 
 pub fn day14_2() {
-    let _inp = "";
+    let _inp = "O....#....
+O.OO#....#
+.....##...
+OO.#O....O
+.O.....O#.
+O.#..O.#.#
+..O..#O..O
+.......O..
+#....###..
+#OO..#....";
     let inp = std::fs::read_to_string("day14.input.txt").unwrap();
     let inp = inp.trim();
 
+    let lines: Vec<_> = inp
+        .lines()
+        .map(|x| x.chars().collect::<Vec<char>>())
+        .collect();
+
+    let lines = cycle(lines, 1000000000);
+    let score = weight(lines);
+    println!("score = {}", score);
+}
+
+#[memoize]
+fn cycle(lines: Vec<Vec<char>>, cycle_count: usize) -> Vec<Vec<char>> {
+    if cycle_count == 0 {
+        lines
+    } else if cycle_count == 1 {
+        tilt_east(tilt_south(tilt_west(tilt_north(lines))))
+    } else if cycle_count == 2 {
+        cycle(cycle(lines, 1), 1)
+    } else if cycle_count % 2 == 0 {
+        cycle(cycle(lines, cycle_count / 2), cycle_count / 2)
+    } else {
+        cycle(cycle(lines, cycle_count - 1), 1)
+    }
+}
+
+fn tilt_west(v: Vec<Vec<char>>) -> Vec<Vec<char>> {
+    let start = v.clone();
+    let tilted = tilt_west_once(v);
+
+    if tilted != start {
+        return tilt_west(tilted);
+    }
+    tilted
+}
+
+fn tilt_west_once(v: Vec<Vec<char>>) -> Vec<Vec<char>> {
+    let mut w = v.clone();
+
+    for j in 0..w[0].len() - 1 {
+        for wi in &mut w {
+            if wi[j] == '.' && wi[j + 1] == 'O' {
+                wi[j + 1] = '.';
+                wi[j] = 'O';
+            }
+        }
+    }
+    w
+}
+
+fn tilt_south(v: Vec<Vec<char>>) -> Vec<Vec<char>> {
+    let start = v.clone();
+    let tilted = tilt_south_once(v);
+
+    if tilted != start {
+        return tilt_south(tilted);
+    }
+    tilted
+}
+
+fn tilt_south_once(v: Vec<Vec<char>>) -> Vec<Vec<char>> {
+    let mut w = v.clone();
+
+    for i in (1..w.len()).rev() {
+        for j in 0..w[i].len() {
+            if w[i][j] == '.' && w[i - 1][j] == 'O' {
+                w[i - 1][j] = '.';
+                w[i][j] = 'O';
+            }
+        }
+    }
+    w
+}
+
+fn tilt_east(v: Vec<Vec<char>>) -> Vec<Vec<char>> {
+    let start = v.clone();
+    let tilted = tilt_east_once(v);
+
+    if tilted != start {
+        return tilt_east(tilted);
+    }
+    tilted
+}
+
+fn tilt_east_once(v: Vec<Vec<char>>) -> Vec<Vec<char>> {
+    let mut w = v.clone();
+    for j in (1..w[0].len()).rev() {
+        for wi in &mut w {
+            if wi[j] == '.' && wi[j - 1] == 'O' {
+                wi[j - 1] = '.';
+                wi[j] = 'O';
+            }
+        }
+    }
+    w
 }
--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ mod day10;
 mod day11;
 mod day12;
 mod day13;
+mod day14;
 mod day2;
 mod day3;
 mod day4;
@@ -13,7 +14,6 @@ mod day6;
 mod day7;
 mod day8;
 mod day9;
-mod day14;
 
 use clap::{Parser, Subcommand};
 use std::fmt;
